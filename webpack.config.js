6b89af6761e8d8f2e9f5038d7b3d685bb8b83f72
Deepak Kannan <kannan.deepak@gmail.com>
2016-07-18 11:01:43 +0530

    Add babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path'),
+      PATHS = {
+        app: path.join(__dirname, 'app'),
+        build: path.join(__dirname, 'build')
+      };
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
